0ce8e1e64a6e755e9a2448914f31b20a32914ec2
HeartlandNetwork <gareth_rowell@nps.gov>
2024-03-21 13:07:07 -0500

    n validation

--- wetlands/src/VIBI-herbaceous/VIBI_herb_end2end.R
+++ wetlands/src/VIBI-herbaceous/VIBI_herb_end2end.R
@@ -69,6 +69,7 @@ glimpse(Access_data)
 #################
 #
 # Step 3 - Substitute NA with -9999 in CoverClass and CoverClassAll
+#  Then remove those with -9999 in CoverClass
 #
 #################
 
@@ -76,6 +77,52 @@ glimpse(Access_data)
 Access_data$CoverClass <- Access_data$CoverClass |> replace_na(-9999)
 
 Access_data$CoverClassAll <- Access_data$CoverClassAll |> replace_na(-9999)
+
+Access_data |>
+  filter(CoverClass == -9999)
+
+Access_data <- Access_data |>
+  filter(CoverClass != -9999)
+
+# then test
+
+Access_data |>
+  filter(CoverClass == -9999)
+
+
+##########
+#
+# Step 3b - check for duplicates
+#
+##########
+
+
+
+# test for dups
+
+
+Access_data |>
+  count(Species, Comments, Module, CoverClass_LT_6m, CoverClassAll,
+        EditDate, HerbSiteName, FeatureID, CoverClass
+  ) |>
+  filter(n > 1)
+
+# Remove dups with distinct() 
+
+Access_data <- Access_data |>
+  distinct(Species, Comments, Module, CoverClass_LT_6m, CoverClassAll,
+           EditDate, HerbSiteName, FeatureID, CoverClass
+  )
+Access_data
+
+# test for dups
+
+
+Access_data |>
+  count(Species, Comments, Module, CoverClass_LT_6m, CoverClassAll,
+        EditDate, HerbSiteName, FeatureID, CoverClass
+  ) |>
+  filter(n > 1)
 
 
 #################
@@ -135,6 +182,11 @@ Access_data <- Access_data |>
   select(EventID, FeatureID, LocationID, Species, Comments, Module,
          CoverClass, CoverClassAll, EditDate )
 
+# writexl::write_xlsx(Access_data, "Load_VIBI_herb_2023.xlsx")
+
+
+
+
 #------------------------------------------------------------------------------
 # End2End test begins here
 
@@ -190,7 +242,15 @@ end2end |>
 # need to delete duplicates from database and reexport <<<<<<<<<<<<<<<<<<<
 # manually delete duplicates from Access_data and retest
 
+# compare record counts
 
+Access_data |>
+  count(EventID, LocationID, Species, ModNo, CovCode)
+
+end2end |>
+  count(EventID, LocationID, Species, ModNo, CovCode)
+
+#view(end2end)
 
 
 
