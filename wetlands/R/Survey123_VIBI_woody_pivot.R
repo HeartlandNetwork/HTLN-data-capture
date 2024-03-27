4529db095194b5aad482f073ae1c989427cbc4b3
HeartlandNetwork <gareth_rowell@nps.gov>
2024-03-27 10:58:06 -0500

    final cleanup

--- wetlands/R/Survey123_VIBI_woody_pivot.R
+++ wetlands/R/Survey123_VIBI_woody_pivot.R
@@ -4,13 +4,68 @@ library(tidyverse)
 
 #setwd("../HTLN-Data-Capture-Scripts/wetlands/src")
 
-load_file <- read_csv("Woody.csv")
 
-glimpse(load_file)
+#setwd("./src")
+
+# load the Survey123 data
+#
+#
+# species codes were only used in CUVA_VIBI_woody1.csv
+# join to create WoodySpecies
+
+load_file1 <- read_csv("CUVA_VIBI_woody1.csv")
+problems(load_file1)
+
+WoodySpecies_LUT <- read_csv("WoodySpecies_LUT2.csv")
+
+load_file1 <- load_file1 |>
+  left_join(WoodySpecies_LUT, join_by(SpeciesCode))
+
+glimpse(load_file1)
+
+glimpse(WoodySpecies_LUT)
+
+view(load_file1)
+
+# check for NAs in WoodySpecies 
+
+
+load_file1 |>
+  select(SpeciesCode, WoodySpecies) |>
+  filter(is.na(WoodySpecies)) |>
+  distinct()
+
+load_file2 <- read_csv("CUVA_VIBI_woody2.csv")
+problems(load_file2)
+load_file3 <- read_csv("CUVA_VIBI_woody3.csv")
+problems(load_file3)
+
+glimpse(load_file1)
+glimpse(load_file2)
+glimpse(load_file3)
+
+Access_data <- bind_rows(load_file1,load_file2)
+
+glimpse(Access_data)
+
+Access_data <- bind_rows(Access_data,load_file3)
+
+load_file <- Access_data
+
+glimpse(Access_data)
+
+
+# record count is 1731. 
+
+# From the spreadsheets: 304 + 280 + 1147 = 1731
+
+304 + 280 + 1147
+
+
 
 # select columns for Access import
 
-Access_data <- load_file |>
+Access_data <- Access_data |>
 	select(WoodyModule, WoodySpecies, EditDate, WoodySiteName, ShrubClump, D0to1,
 	       D1to2_5, D2_5to5, D5to10, D10to15, D15to20, D20to25, D25to30, D30to35,
 	       D35to40, Dgt40, Dgt40_1, Dgt40_2, Dgt40_3, Dgt40_4, Dgt40_5)
@@ -50,10 +105,11 @@ Access_data <- Access_data |>
 glimpse(Access_data)
 
 
+#<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # create the LocationID column from the FeatureID column
 # and a lookup table from HTLNWetlands
 
-Locations_LUT <- read_csv("Locations_LUT.csv")
+Locations_LUT <- read_csv("tbl_Locations_20230316.csv")
 
 glimpse(Locations_LUT)
 
@@ -62,27 +118,30 @@ Access_data <- Access_data |>
 
 glimpse(Access_data)
 
-# create Scientific_Name column from WoodySpecies codes
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-WoodySpecies_LUT <- read_csv("WoodySpecies_LUT.csv")
+# create Scientific_Name column from WoodySpecies codes 
+
+WoodySpecies_LUT <- read_csv("WoodySpecies_LUT2.csv")
 
 glimpse(WoodySpecies_LUT)
 
 Access_data <- Access_data |>
   left_join(WoodySpecies_LUT, join_by(WoodySpecies))
 
+# does the above code through many-to-many warning?? <<<<<<<<<<<<<<<<
+# see WoodySpecies_LUT cleanup script
+
 glimpse(Access_data)
 
 # set up columns before normalization
 
 Access_data <- Access_data |>
-	select(EventID, LocationID, FeatureID, Module_No, Scientific_Name, 
+	select(EventID, LocationID, FeatureID, Module_No, WoodySpecies, 
 	       EditDate, WoodySiteName, Col1, Col2, Col3, Col4, Col5,
 	       Col6, Col7, Col8, Col9, Col10, Col11, Col12)
 
 glimpse(Access_data)
-
-
 # pivot longer (normalize)
 
 Access_data <- Access_data |> 
@@ -96,6 +155,8 @@ Access_data <- Access_data |>
 glimpse(Access_data)
 
 
+## <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 # Join the diameter information from a LUT
 
 Diam_LUT <- read_csv("Diam_LUT.csv")
@@ -107,6 +168,7 @@ Access_data <- Access_data |>
 
 # Need an end-to-end test after all the column manipulations -------------------
 # Sum of counts in initial load file
+
 
 Initial_load <- load_file |>
   select(ShrubClump, D0to1,
@@ -124,6 +186,8 @@ Access_data |>
     total_count = sum(Count)
   )
 
+# Passed this test
+
 #-------------------------------------------------------------------------------
 
 # Substitute NA with -9999 in Count data 
@@ -136,9 +200,14 @@ Access_data$Count <- Access_data$Count |> replace_na(-9999)
 
 Access_data <- Access_data |>
   select( EventID, LocationID, FeatureID, Module_No, 
-          Scientific_Name, Diam_Code, Count
+          WoodySpecies, Diam_Code, Count
   )
 
 glimpse(Access_data)
+view(Access_data)
 
-writexl::write_xlsx(Access_data, "Load_VIBI_woody.xlsx")
+# writexl::write_xlsx(Access_data, "Load_VIBI_woody.xlsx")
+
+
+
+
